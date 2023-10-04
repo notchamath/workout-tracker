8a5171130077e921fc9e81d4d58d4e70d995b534
Chamath Jayarathne <chamathsocial@gmail.com>
2023-10-04 15:57:53 -0400

    fix: Add more details about myself to get more accurate calorie and workout tracking from api

--- main.py
+++ main.py
@@ -2,19 +2,33 @@ import requests
 import os
 from dotenv import load_dotenv
 
+# API INFO
 load_dotenv(".env")
 APP_ID = os.getenv("APP_ID")
 API_KEY = os.getenv("API_KEY")
 API_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
+# YOUR INFO
+SEX = os.getenv("SEX")
+WEIGHT_KG = os.getenv("WEIGHT_KG")
+HEIGHT_CM = os.getenv("HEIGHT_CM")
+AGE = os.getenv("AGE")
+
+# Describe Workout
+TODAYS_WORKOUT = "Lifted weights for 2 hours"
+
 HEADERS = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
-    "x-remote-user-id": "0"
+    "x-remote-user-id": "0",
 }
 
 params = {
-    "query": "Lifted weights for 2 hours"
+    "query": TODAYS_WORKOUT,
+    "gender": SEX,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
 }
 
 res = requests.post(url=API_ENDPOINT, headers=HEADERS, json=params)
